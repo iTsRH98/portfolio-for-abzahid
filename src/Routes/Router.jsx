ae4a5d137fda68be52c1891fde762e0b6348346a
iTsRH98 <rhnayemfardin@gmail.com>
2025-05-09 23:47:08 +0600

    added navbar and completed routing setup

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,7 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout/MainLayout";
-import Home from "../components/Home/Home";
+import Home from "../pages/Home/Home";
 import ErrorPage from "../components/ErrorPage/ErrorPage";
+import About from "../pages/About/About";
+import Skills from "../pages/Skills/Skills";
+import Find from "../pages/Find/Find";
+import Contact from "../pages/Contact/Contact";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +17,22 @@ const router = createBrowserRouter([
         index: true,
         path: "/",
         Component: Home,
+      },
+      {
+        path: "/about",
+        Component: About,
+      },
+      {
+        path: "/skills",
+        Component: Skills,
+      },
+      {
+        path: "/contact",
+        Component: Contact,
+      },
+      {
+        path: "/find",
+        Component: Find,
       },
     ],
   },
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
 
 const Header = () => {
   return (
     <div>
-      <h1>this is header</h1>
+      <Navbar></Navbar>
     </div>
   );
 };
